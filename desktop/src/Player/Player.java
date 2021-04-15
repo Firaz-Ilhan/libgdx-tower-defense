8b44f23cd5bed207939c24ababa7447f13b2df00
Lea Gutierrez Lange <lg067@hdm-stuttgart.de>
2021-04-15 15:10:50 +0200

    dummy implementaion of a Player Class

--- desktop/src/Player/Player.java
+++ desktop/src/Player/Player.java
@@ -1,10 +1,15 @@
 package Player;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-    String name;
-    int wallet = 0;
-    boolean right;
-    boolean left;
+    private String name;
+    private int wallet = 0;
+    private boolean right;
+    private boolean left;
+    private int lifepoints = 200;
+   // private List<ITower> inventory;
 
     public Player(String name, boolean left,boolean right){
         this.name= name;
@@ -16,15 +21,35 @@ public class Player {
         reward= reward - enemiesMissed*2; //if you missed enemies, you get less money at the end of the round
         wallet+=reward;
     }
-  /**
-   * Because there is no ITower by now, I commented this method
-   *
-     public void placeTower(ITower tower, int x,int y){
+
+
+    /**
+     * Because there is no ITower by now, I commented this method
+     *
+     public List<ITower> getInventory(){
+     return List.copyOf(inventory);
+     }
+
+    public void buyTower(ITower tower){
+        inventory.add(tower);
         wallet = wallet - tower.getCost;
+    }
+    public void sellTower(int i){
+        inventory.remove(i);
+        wallet = wallet + tower.getCost*0.8; //selling Towers only regain 80% of the costs
+    }
+
+     public void placeTower(ITower tower, int x,int y){
         //the command for communication with the server must be implemented here
     }
  */
     public int getWalletValue(){
         return wallet;
+    }
+    public void reduceLifepoints(int damage){
+        lifepoints = lifepoints-damage;
+    }
+    public int getLifepoints(){
+        return lifepoints;
     }
 }

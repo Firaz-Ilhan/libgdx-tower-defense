37d404b96fae4c499209421bc383f2e2af1ab3a2
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-04-19 13:36:52 +0200

    add an asset manager(#8)

--- core/src/com/tower/defence/TowerDefense.java
+++ core/src/com/tower/defence/TowerDefense.java
@@ -1,20 +1,33 @@
 package com.tower.defence;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.tower.defence.screen.MainMenuScreen;
 
 
 public class TowerDefense extends Game {
 
+    public AssetManager assetManager;
+
+    @Override
     public void create() {
+        //loading assets
+        assetManager = new AssetManager();
+        assetManager.load("skins/glassyui/glassy-ui.json", Skin.class);
+        assetManager.finishLoading();
+
         //set first screen to main menu
-        this.setScreen(new MainMenuScreen());
+        this.setScreen(new MainMenuScreen(this));
     }
 
+    @Override
     public void render() {
         super.render();
     }
 
+    @Override
     public void dispose() {
+        assetManager.dispose();
     }
 }
--- core/src/com/tower/defence/screen/MainMenuScreen.java
+++ core/src/com/tower/defence/screen/MainMenuScreen.java
@@ -10,12 +10,17 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.tower.defence.TowerDefense;
 
 public class MainMenuScreen implements Screen {
 
     private final Stage stage;
+    private Skin skin;
+    private TowerDefense game;
 
-    public MainMenuScreen() {
+    public MainMenuScreen(TowerDefense game) {
+        this.game = game;
+        skin = game.assetManager.get("skins/glassyui/glassy-ui.json");
         //create stage and set it as input processor
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
@@ -23,12 +28,10 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void show() {
-        final Table table = new Table();
-        table.setFillParent(true);
-        table.setDebug(false);
-        stage.addActor(table);
-
-        Skin skin = new Skin(Gdx.files.internal("skins/glassyui/glassy-ui.json"));
+        final Table menuTable = new Table();
+        menuTable.setFillParent(true);
+        menuTable.setDebug(false);
+        stage.addActor(menuTable);
 
         //create buttons
         final TextButton multiplayerButton = new TextButton("Multiplayer", skin, "default");
@@ -37,14 +40,14 @@ public class MainMenuScreen implements Screen {
         final TextButton exitButton = new TextButton("Exit", skin, "default");
 
         //add buttons to table
-        table.defaults().pad(10f);
-        table.add(multiplayerButton);
-        table.row();
-        table.add(settingsButton);
-        table.row();
-        table.add(aboutButton);
-        table.row();
-        table.add(exitButton);
+        menuTable.defaults().pad(10f);
+        menuTable.add(multiplayerButton);
+        menuTable.row();
+        menuTable.add(settingsButton);
+        menuTable.row();
+        menuTable.add(aboutButton);
+        menuTable.row();
+        menuTable.add(exitButton);
 
         //create button listeners
         exitButton.addListener(new ChangeListener() {
@@ -54,11 +57,18 @@ public class MainMenuScreen implements Screen {
             }
         });
 
+        settingsButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new SettingsScreen(game));
+            }
+        });
+
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.draw();
     }

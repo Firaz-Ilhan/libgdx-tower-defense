59031ff95b78fa6f0248b4fa26e520679e9d9e36
Niklas Mäckle <nm067@hdm-stuttgart.de>
2021-04-23 14:10:43 +0200

    removed unwanted changes in DesktopLauncher.java

--- desktop/src/com/tower/defence/desktop/DesktopLauncher.java
+++ desktop/src/com/tower/defence/desktop/DesktopLauncher.java
@@ -3,16 +3,14 @@ package com.tower.defence.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.tower.defence.TowerDefense;
-import com.tower.defence.Map.TD_map;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Tower Defense";
-		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
-        config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
-		config.resizable = false;
-		config.fullscreen = false;
+		config.width = 1600;
+		config.height = 900;
+		config.fullscreen = true;
 		config.vSyncEnabled = true;
 		config.foregroundFPS = 60;
 		config.backgroundFPS = 30;

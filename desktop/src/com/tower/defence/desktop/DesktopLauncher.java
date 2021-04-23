e3222543aee557150c059a3e9e2686222dfd61df
Niklas Mäckle <nm067@hdm-stuttgart.de>
2021-04-23 14:06:56 +0200

    changed screen resolution to fit map size

--- desktop/src/com/tower/defence/desktop/DesktopLauncher.java
+++ desktop/src/com/tower/defence/desktop/DesktopLauncher.java
@@ -3,14 +3,16 @@ package com.tower.defence.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.tower.defence.TowerDefense;
+import com.tower.defence.Map.TD_map;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Tower Defense";
-		config.width = 1920;
-		config.height = 1080;
-		config.fullscreen = true;
+		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
+        config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
+		config.resizable = false;
+		config.fullscreen = false;
 		config.vSyncEnabled = true;
 		config.foregroundFPS = 60;
 		config.backgroundFPS = 30;

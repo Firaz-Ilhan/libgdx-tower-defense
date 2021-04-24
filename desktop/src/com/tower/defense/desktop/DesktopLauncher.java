b1902790248410ed8f08781b2a394fc933ca6e35
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-04-24 20:37:51 +0200

    switch to LWJGL3 backend(#4)

--- desktop/src/com/tower/defense/desktop/DesktopLauncher.java
+++ desktop/src/com/tower/defense/desktop/DesktopLauncher.java
@@ -1,19 +1,18 @@
 package com.tower.defense.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.tower.defense.TowerDefense;
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
-        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = "Tower Defense";
-        config.width = 1600;
-        config.height = 900;
-        config.fullscreen = false;
-        config.vSyncEnabled = true;
-        config.foregroundFPS = 60;
-        config.backgroundFPS = 30;
-        new LwjglApplication(new TowerDefense(), config);
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Tower Defense");
+        config.setWindowedMode(1600, 900);
+        config.useVsync(true);
+        config.setForegroundFPS(60);
+        config.setIdleFPS(30);
+        new Lwjgl3Application(new TowerDefense(), config);
+
     }
 }

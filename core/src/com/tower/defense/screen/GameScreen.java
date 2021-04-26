7b91cffd49eada373719282db2112030a1b82d39
Niklas Mäckle <nm067@hdm-stuttgart.de>
2021-04-26 11:41:34 +0200

    fixed a bug where the hoveredTile position would be at the wrong position on the screen

--- core/src/com/tower/defense/screen/GameScreen.java
+++ core/src/com/tower/defense/screen/GameScreen.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.tower.defense.TowerDefense;
@@ -32,14 +33,13 @@ public class GameScreen implements Screen{
     private SpriteBatch spriteBatch;
 
     private Vector2 mousePosition;
-
-    private int tileX;
-    private int tileY;
+    private Vector2 hoveredTilePosition;
+    private Vector3 touchPos;
 
     //Help variables to show mouse position
     private BitmapFont font;
 
-    private Texture hoveredTile;
+    private Texture hoveredTileTexture;
     private Texture turret;
 
     public GameScreen(TowerDefense game) {
@@ -52,7 +52,7 @@ public class GameScreen implements Screen{
     @Override
     public void show() {
         map = new TmxMapLoader().load("map/TowerDefenseMapPrototype.tmx");
-        hoveredTile = new Texture(Gdx.files.internal("hovered_tile.png"));
+        hoveredTileTexture = new Texture(Gdx.files.internal("hovered_tile.png"));
         //temporary
         turret = new Texture(Gdx.files.internal("turret.png"));
 
@@ -91,13 +91,16 @@ public class GameScreen implements Screen{
         camera.update();
         renderer.setView(camera);
 
-        //getting the current mouse position
-        mousePosition = new Vector2(Gdx.input.getX(),Gdx.graphics.getHeight() - Gdx.input.getY());
+        //touchPos is used to translate the screen coordinates to the world coordinates
+        touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+        camera.unproject(touchPos);
 
-        //x position of hovered tile
-        tileX = (int) mousePosition.x / 50;
-        //y position of hovered tile
-        tileY = (int) mousePosition.y / 50;
+        //getting the current mouse position
+        //mousePosition = new Vector2(Gdx.input.getX(),Gdx.graphics.getHeight() - Gdx.input.getY());
+        mousePosition = new Vector2((int)touchPos.x, (int)touchPos.y);
+
+        //position of the hovered tile
+        hoveredTilePosition = new Vector2((int) mousePosition.x / 50, (int) mousePosition.y / 50);
 
         renderer.getBatch().begin();
 
@@ -106,8 +109,8 @@ public class GameScreen implements Screen{
         //temporary help
         font.draw(renderer.getBatch(),String.valueOf(mousePosition.x),0,40);
         font.draw(renderer.getBatch(),String.valueOf(mousePosition.y),100,40);
-        font.draw(renderer.getBatch(),String.valueOf(tileX),0,100);
-        font.draw(renderer.getBatch(),String.valueOf(tileY),100,100);
+        font.draw(renderer.getBatch(),String.valueOf(hoveredTilePosition.x),0,100);
+        font.draw(renderer.getBatch(),String.valueOf(hoveredTilePosition.y),100,100);
 
         renderer.getBatch().end();
         
@@ -116,13 +119,14 @@ public class GameScreen implements Screen{
 
         //rendering the hoveredTile visually on top of all tiles
         spriteBatch.begin();
-        spriteBatch.draw(hoveredTile, tileX * 50, tileY * 50);
+        //TODO if( )
+        spriteBatch.draw(hoveredTileTexture, hoveredTilePosition.x * 50, hoveredTilePosition.y * 50);
         spriteBatch.end();
 
         //temporary left click method to show the turret
         if(Gdx.input.isButtonPressed(Buttons.LEFT)){
             spriteBatch.begin();
-            spriteBatch.draw(turret, tileX * 50, tileY * 50);
+            spriteBatch.draw(turret, hoveredTilePosition.x * 50, hoveredTilePosition.y * 50);
             spriteBatch.end();
         }  
 

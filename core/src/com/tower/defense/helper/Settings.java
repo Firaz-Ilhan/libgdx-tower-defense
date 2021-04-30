2ff96d93570f6595b2a5397c50905df9de6337ca
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2021-04-30 11:26:20 +0200

    add methods to store user settings(#9)
    and apply changes to TowerDefense and SettingsScreen

--- core/src/com/tower/defense/helper/Settings.java
+++ core/src/com/tower/defense/helper/Settings.java
@@ -1,22 +1,50 @@
 package com.tower.defense.helper;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 
 /**
- * Only static access allowed
+ * Settings are stored in...
+ * Windows: %UserProfile%/.prefs/Tower_Defense_Settings
+ * Linux and OS X: ~/.prefs/Tower_Defense_Settings
  */
-public final class Settings {
+public class Settings {
 
-    //prevents class from being instantiated
-    private Settings() {
+    private final String fullscreen = "fullscreen";
 
+    public Preferences getPreferences() {
+        final String fileName = "Tower_Defense_Settings";
+        return Gdx.app.getPreferences(fileName);
     }
 
-    public static void toggleDisplayMode() {
-        if (Gdx.graphics.isFullscreen()) {
-            Gdx.graphics.setWindowedMode(1600, 900);
-        } else {
+    /**
+     * activates or deactivates the full screen mode in the settings file
+     *
+     * @param value true to activate the full screen mode
+     */
+    public void setFullscreenMode(boolean value) {
+        getPreferences().putBoolean(fullscreen, value);
+        getPreferences().flush();
+    }
+
+    /**
+     * checks whether the full screen mode in the settings file is enabled or not
+     *
+     * @return true if fullscreen is enabled in the settings file
+     */
+    public boolean isFullscreenEnabled() {
+        return getPreferences().getBoolean(fullscreen);
+    }
+
+    /**
+     * switches between full screen and windowed mode, depending on the
+     * current value in the settings file
+     */
+    public void toggleDisplayMode() {
+        if (isFullscreenEnabled()) {
             Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+        } else {
+            Gdx.graphics.setWindowedMode(1600, 900);
         }
     }
 }

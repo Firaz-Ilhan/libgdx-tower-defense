858a60ed6691136f6cbfe7b25fc52962eaba7d23
TheMeinGamer <themeingamer@outlook.com>
2021-05-14 12:59:34 +0200

    Added Classes from local Repo

---
+++ core/src/com/tower/defense/network/packet/server/PacketOutChatMessage.java
@@ -0,0 +1,50 @@
+package com.tower.defense.network.packet.server;
+
+import org.json.JSONObject;
+
+import com.tower.defense.network.packet.Packet;
+import com.tower.defense.network.packet.PacketType;
+
+
+
+public class PacketOutChatMessage extends Packet {
+	
+	private String name;
+	private String text;
+	
+	public PacketOutChatMessage() {
+	}
+	
+	public PacketOutChatMessage(String name, String text) {
+		this.packetType = PacketType.getPacketTypeByClass(getClass());
+		this.name = name;
+		this.text = text;
+	}
+	
+	@Override
+	public JSONObject read() {
+		JSONObject object = new JSONObject();
+		object.put("id", packetType.getPacketID());
+		object.put("name", name);
+		object.put("text", text);
+		return object;
+	}
+	
+	@Override
+	public void write(JSONObject object) {
+		this.packetType = PacketType.getPacketTypeByID(object.getInt("id"));
+		this.name = object.getString("name");
+		this.text = object.getString("text");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	
+
+}
